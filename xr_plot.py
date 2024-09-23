1608cec4f48ccd3b785a6bf55dbce482e37c1ddc
niknovikov19 <nikknovikov@gmail.com>
2024-09-23 18:48:20 -0400

    Create repository

---
+++ xr_plot.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import hsv_to_rgb
+import matplotlib.colors as mcolors
+import matplotlib.colorbar as mcolorbar
+import numpy as np
+import xarray as xr
+
+
+def plot_xarray_2d(X: xr.DataArray, need_log=False, show_ax_names=True,
+                   clim=(None, None)):
+    coord_x = X.dims[1]
+    coord_y = X.dims[0]
+    cx = X.coords[coord_x].values
+    cy = X.coords[coord_y].values
+    ext = [cx[0], cx[-1], cy[-1], cy[0]]
+    X_ = X.values
+    if need_log:
+        X_ = np.log(X_)
+    plt.imshow(X_, aspect='auto', extent=ext, origin='upper',
+               vmin=clim[0], vmax=clim[1])
+    plt.xlim(cx[0], cx[-1])
+    plt.ylim(cy[-1], cy[0])
+    if show_ax_names:
+        plt.xlabel(coord_x)
+        plt.ylabel(coord_y)
+
+def plot_xarray_2d_irreg(X: xr.DataArray, need_log=False, cmap=None):
+    coord_x = X.dims[1]
+    coord_y = X.dims[0]
+    cx = X.coords[coord_x].values
+    cy = X.coords[coord_y].values
+    Cx, Cy = np.meshgrid(cx, cy)
+    ax = plt.gca()
+    X_ = X.values
+    if need_log:
+        X_ = np.log(X_)
+    if cmap is not None:
+        cmap = plt.get_cmap(cmap)
+    m = ax.pcolormesh(Cx, Cy, X_, shading='auto', cmap=cmap)
+    #ax.set_xscale('log')
+    ax.invert_yaxis()
+    plt.xlabel(coord_x)
+    plt.ylabel(coord_y)
+    plt.colorbar(m)
+    plt.title(X.name)
---
+++ xr_spect.py
@@ -0,0 +1,101 @@
+import numpy as np
+import scipy.signal as sig
+import xarray as xr
+
+
+def calc_xr_welch(X_in: xr.DataArray, win_len=0.5, win_overlap=0.5, fmax=100,
+                  fs=None, time_dim='time'):
+    """Calculate power using Welch method. """
+    
+    # The code below assumes that time is the last dimension
+    if X_in.dims[-1] != time_dim:
+        raise ValueError('Time should be the last dimension')
+    
+    # Sampling rate
+    tt0 = X_in.coords[time_dim].values
+    if fs is None:
+        fs = round(1. / (tt0[1] - tt0[0]), 5)  # round to correct for numerical errors
+
+    # Window and overlap in samples
+    nperseg = round(win_len * fs)
+    noverlap = round(win_overlap * win_len * fs)
+
+    # Call welch() on a surrogate array to get the output frequencies
+    xz = np.zeros(len(tt0))
+    ff, _ = sig.welch(xz, fs=fs, nperseg=nperseg, noverlap=noverlap)
+
+    # Wrapping function for welch() that returns a single variable (power)
+    def f(X, fs, nperseg, noverlap):
+        _, S = sig.welch(
+            X, fs=fs, nperseg=nperseg, noverlap=noverlap, axis=-1)
+        return S
+    
+    # Apply welch() to xr.DataArray (with dask support)
+    W = xr.apply_ufunc(
+        f, X_in,
+        kwargs={'fs': fs, 'nperseg': nperseg, 'noverlap': noverlap},
+        input_core_dims=[[time_dim]],
+        output_core_dims=[['freq']],
+        output_sizes={'freq': len(ff)},
+        vectorize=False, dask='parallelized',
+        output_dtypes=[np.float64]
+    )
+    W = W.assign_coords({'freq': ('freq', ff)})
+    
+    # Select freq. range of interest
+    W = W.sel(freq=slice(None, fmax))
+    return W
+    
+
+def calc_xr_cpsd(X1_in: xr.DataArray, X2_in: xr.DataArray, 
+                 win_len=0.5, win_overlap=0.5, fmax=100,
+                 fs=None, time_dim='time'):
+    """Cross-power spectral density between two signals, complex-valued. """
+
+    # The code below assumes that time is the last dimension
+    if (X1_in.dims[-1] != time_dim) or (X2_in.dims[-1] != time_dim):
+        raise ValueError('Time should be the last dimension')
+    
+    # Sampling rate
+    tt0 = X1_in.coords[time_dim].values
+    if fs is None:
+        fs = round(1. / (tt0[1] - tt0[0]), 5)  # round to correct for numerical errors
+
+    # Window and overlap in samples
+    nperseg = round(win_len * fs)
+    noverlap = round(win_overlap * win_len * fs)
+
+    # Call sig.csd() on a surrogate array to get the output frequencies
+    # Function name csd() is misleading and means cross-spetral density
+    xz = np.zeros(len(tt0))
+    ff, _ = sig.csd(xz, xz, fs=fs, nperseg=nperseg, noverlap=noverlap, axis=-1)
+
+    # Wrapping function for sig.csd() that returns a single variable
+    def f(X1, X2, fs, nperseg, noverlap):
+        _, S = sig.csd(
+            X1, X2, fs=fs, nperseg=nperseg, noverlap=noverlap, axis=-1)
+        return S
+    
+    # Apply sig.csd() to xr.DataArray's (with dask support)
+    W = xr.apply_ufunc(
+        f, X1_in, X2_in,
+        kwargs={'fs': fs, 'nperseg': nperseg, 'noverlap': noverlap},
+        input_core_dims=[[time_dim], [time_dim]],
+        output_core_dims=[['freq']],
+        output_sizes={'freq': len(ff)},
+        vectorize=False, dask='parallelized',
+        output_dtypes=[np.float64]
+    )
+    W = W.assign_coords({'freq': ('freq', ff)})
+    
+    # Select freq. range of interest
+    W = W.sel(freq=slice(None, fmax))
+    return W
+
+
+# =============================================================================
+# dt = 1e-3
+# X = xr.DataArray(np.zeros((5, 1000)), dims=['depth', 'time'], 
+#                  coords={'depth': np.arange(5), 'time': dt * np.arange(1000)})
+# W = calc_xr_cpsd(X, X.isel(depth=0))
+# =============================================================================
